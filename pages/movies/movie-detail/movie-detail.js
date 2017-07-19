c3c2faca8acdc628a046573d413b7e302a0058ed
lobawzhw <88075056@qq.com>
2017-07-19 23:45:29 +0800

    movie-detail

--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -1,18 +1,29 @@
-// pages/movies/movie-detail/movie-detail.js
+var utils = require('../../../utils/utils.js');
+var app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
     var id = options.id;
-    console.log(id);
+    // console.log(id);
+    var url = app.globalData.doubanBase + '/v2/movie/subject/' + id;
+    utils.http(url, this.processDoubanData);
   },
+
+  processDoubanData:function(data) {
+    // this.setData(data);
+    console.log(data);
+  }
+
 })
